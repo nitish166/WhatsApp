c50205ef1d3934ad44be49a2162b00c99ae40543
unknown <knitish123@gmail.com>
2021-09-21 16:53:55 +0530

    Whatsapp left-chat

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJD48MJYinyk0J_0wGnSo8sbC7UOcIaiQ",
+  authDomain: "whats-app-clone-13a44.firebaseapp.com",
+  projectId: "whats-app-clone-13a44",
+  storageBucket: "whats-app-clone-13a44.appspot.com",
+  messagingSenderId: "273122475612",
+  appId: "1:273122475612:web:7430cb70edfe0c4d51f099",
+  measurementId: "G-WE4BBMY67W",
+};
