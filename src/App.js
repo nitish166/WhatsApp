53750caa1257e1bb9eae88a3103221648130879c
unknown <knitish123@gmail.com>
2021-09-25 09:08:59 +0530

    Google API Authentication

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import React from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     // BEM Naming convention
     <div className="app">
-      <div className="app__body">
-        <Router>
-          <Sidebar />
-          <Switch>
-            <Route path="/Rooms/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <Chat />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+      {!user ? (
+        <h1>LOGIN</h1>
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/Rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
